51d9ce06174c4b73d063a041e08c0bcfc0dcb051
khatskevich <avkhatskevich@gmail.com>
2017-05-15 15:31:10 +0300

    pass tarantul src as arg to try.sh

--- try.sh
+++ try.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-TARANTOOL_SRC="../tarantool/"
+TARANTOOL_SRC=$1
 TARANTOOL_BIN="$TARANTOOL_SRC/src/tarantool"
 TARANTOOL_TEST="$TARANTOOL_SRC/test/"
-#TCL_DIR="$TARANTOOL_TEST/sqlite-tcl/"
 OUTPUT_DIR="$TARANTOOL_TEST/sql-tap-try/"
 SQL_TAP_DIR="$TARANTOOL_TEST/sql-tap/"
 if ! [ -d $OUTPUT_DIR ]; then
@@ -10,7 +9,7 @@ if ! [ -d $OUTPUT_DIR ]; then
   cp -r $SQL_TAP_DIR/lua $OUTPUT_DIR/
   cp -r $SQL_TAP_DIR/suite.ini $OUTPUT_DIR/
 fi
-$TARANTOOL_BIN tcl2.lua $1 > $OUTPUT_DIR/$(basename $1)   
-chmod +x $OUTPUT_DIR/$(basename $1)   
+$TARANTOOL_BIN tcl2.lua $2 > $OUTPUT_DIR/$(basename $2)   
+chmod +x $OUTPUT_DIR/$(basename $2)   
 cd $TARANTOOL_TEST && ./test-run.py $(basename $OUTPUT_DIR) --force
 
