2f6fedeeda28511dd3607ed036bb29242a6bf496
khatskevich <avkhatskevich@gmail.com>
2017-10-02 14:41:04 +0300

    fix try.sh so that it works with oos build

--- try.sh
+++ try.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+if [ "$#" -lt 3 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage:"
+    echo "./try.sh TARANTOOL_SRC_PATH TRANTOOL_BUILDDIR test_to convert  [test to convert ...]"
+fi
+
 TARANTOOL_SRC=$1
-TARANTOOL_BIN="$TARANTOOL_SRC/src/tarantool"
+TARANTOOL_BUILDDIR=$2
+TARANTOOL_BUILDDIR=$(readlink -f $TARANTOOL_BUILDDIR)
+TARANTOOL_BIN=$2/src/tarantool
 TARANTOOL_TEST="$TARANTOOL_SRC/test/"
 OUTPUT_DIR="$TARANTOOL_TEST/sql-tap-try/"
 SQL_TAP_DIR="$TARANTOOL_TEST/sql-tap/"
@@ -11,7 +20,8 @@ if ! [ -d $OUTPUT_DIR ]; then
   cp -r $SQL_TAP_DIR/suite.ini $OUTPUT_DIR/
 fi
 
-shift
+shift # shift tarantools src arg
+shift # shift tarantool builddir
 
 while (( "$#" )); do
   python uncoment_must_work.py $1 > $TMP_FILE
@@ -21,5 +31,5 @@ while (( "$#" )); do
   shift
 done
 echo "Lol"
-cd $TARANTOOL_TEST && ./test-run.py $(basename $OUTPUT_DIR) --force
+cd $TARANTOOL_TEST && ./test-run.py --builddir $TARANTOOL_BUILDDIR $(basename $OUTPUT_DIR) --force
 
